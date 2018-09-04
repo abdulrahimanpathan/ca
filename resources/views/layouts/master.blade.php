11c885c00507074a042b04fb52f90de809260fd8
abdulrahimanpathan <rahiman.dcet@gmail.com>
2018-09-04 18:50:57 +0530

    menu added for all the pages

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,9 +6,15 @@
 </head>
 <body>
 
-
 <div class="container">
 <h2 style="text-align: center;"><a href="/">Cricket Assignment</a></h2><br/>
+<div class="links" style="padding-left: 16px">
+    <a href="/teams">Teams</a>&nbsp&nbsp&nbsp
+    <a href="/teams/create">Add Team</a>&nbsp&nbsp&nbsp
+    <a href="/players/create">Add Player</a>&nbsp&nbsp&nbsp
+    <a href="/fixtures">Fixtures</a>&nbsp&nbsp&nbsp
+    <a href="/points-table">Points Table</a>
+</div>
     @yield('content')
 </div>
 
